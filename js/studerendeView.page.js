4c24b25bf33da9862e0c28a956a7cb9f8882105f
benjikyster <benjamin.h.kyster@gmail.com>
2016-11-24 14:08:04 +0100

    Oversigt over kurser

--- js/studerendeView.page.js
+++ js/studerendeView.page.js
@@ -5,25 +5,22 @@ $(document).ready(function () {
 
     $.ajax({
         type: 'GET',
-        url: SDK.serverURL + "/getCourses/" + "6",
+        url: SDK.serverURL + "/course/" + SDK.Storage.load("userId"),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (course) {
-            console.log(course);
+            var decrypted = $.parseJSON(SDK.Decrypt(course))
 
+            decrypted.forEach(function (decrypted){
 
-            course.forEach(function (course) {
-
-                $studentTableBody.append(
+                studentTableBody.append(
                     "<tr>" +
-                    "<td>" + course.code + "</td>" +
+                    "<td>" + decrypted.code + "</td>" +
                     "<td>" + /*course.reviewAverage*/ +"</td>" +
                     "<td>" + /*button*/ +"</td>" +
                     "</tr>"
                 );
-
             });
-
         },
         error: function (course) {
             alert('Failed!');
